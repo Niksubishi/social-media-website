2c5b63d25668efdefcfd948548edadab8a2c11a1
Niksubishi <nikbishopdesign@gmail.com>
2025-01-26 18:51:14 +0100

    Config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}", "./**/*.html"],
+  base: "/social-media-website/",
   theme: {
     extend: {
       colors: {
